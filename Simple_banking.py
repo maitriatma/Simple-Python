e5f788e87b2020ddd7749ac7415a5b1a90f5b055
Karan Sunar <128043060+maitriatma@users.noreply.github.com>
2023-04-17 17:50:58 +0545

    Update Simple_banking.py

--- Simple_banking.py
+++ Simple_banking.py
@@ -53,7 +53,7 @@ def main():
             break
             
         else:
-            print("Invalid choice. Please try again.")
+            print("Invalid choice!. Please try again.")
             
 if __name__ == "__main__":
     main()
